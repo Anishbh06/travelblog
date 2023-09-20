6cc28564e3ac87e4a347a7ba3b6c524a89b1d85a
adn0n <adn0n.akr@gmail.com>
2023-09-20 19:58:14 +0530

    port

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3000;
+const PORT = 10000;
 
 const transporter = nodemailer.createTransport({
   host: 'sandbox.smtp.mailtrap.io',
